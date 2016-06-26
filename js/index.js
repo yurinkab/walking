0079bce7e6aab76fb2ae7659316e77650fdbde0e
yurinkab <yurinkab@gmail.com>
2016-06-26 20:58:19 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+var tipbox=document.querySelector('.tip');
+tipbox.addEventListener('click', function(){
+  $(".tip").hide();
+});
 var count = 0;
 var hoursCount = 0;
-
+var firstTime=false;
 var first= false;
 var addH = document.querySelector('.check');
 addH.innerHTML = "You will See Here Your Statistics"
@@ -8,9 +12,20 @@ var main = document.getElementById("main");
 var isChecked = false;
 var divOld = document.getElementById("div")
 
-function countUp(){
-  count = count + 5;
+function minutesUp(){
+  count+= 5;
+    addH.innerHTML = "You Walked: "+count+" minutes out of "+hoursCount + " Minutes";
+    if (count >= 10) {
+  $(".walking").css({opacity: "0.3"});
 }
+  if (count >= 20) {
+  $(".walking").css({opacity: "0.7"});
+}
+  if (count >= 30) {
+  $(".walking").css({opacity: "1"});
+}
+}
+
 
 function checkTime() {
 var date = new Date();
@@ -24,45 +39,24 @@ var s = date.getSeconds();
     s = "0" + s;
   }
 
-  if(first === false) {
-          var div = document.createElement('div');
-  div.innerHTML = "<h3>Did you Walk 5 Minutes at:</h3><b><p class='time'> " + h + ':' + m + ':' + s + "</b></p><form action='#'><input type='radio' name='walk' value='yes' onclick='countUp()'>Yes!<br><input type='radio'  name='walk' value='no' >No. :(<br></form>";
-div.className = 'box';
-  $( "#main" ).prepend( div );
-    first = true;
-    hoursCount+= 30;
-  }
-  
-var shoot = new Audio("https://raw.githubusercontent.com/yurinkab/game-boxhit/master/lose.wav");
-  
-
-  
-if(s % 10 === 0) {
-
   console.log("its been 30 Minutes! Start Walking!!!");
 
 
       var div = document.createElement('div');
-  div.innerHTML = "<h3>Did you Walk 5 Minutes at:</h3><b><p class='time'> " + h + ':' + m + ':' + s + "</b></p><form action='#'><input type='radio' name='walk' value='yes' onclick='countUp()'>Yes!<br><input type='radio'  name='walk' value='no' >No. :(<br></form>";
+  div.innerHTML = "<b><p class='time'> " + h + ':' + m + ':' + s + "</b></p><h3>Time to Walk! <br>Did you Walk 5 Minutes?</h3><form action='#'><input type='radio' name='walk' value='yes' onclick='minutesUp()'>Yes<br><input type='radio'  name='walk' value='no' >No<br></form>";
 div.className = 'box';
-    if (count >= 10) {
-  $(".walking").css({opacity: "0.3"});
-}
-  if (count >= 20) {
-  $(".walking").css({opacity: "0.7"});
-}
-  if (count >= 30) {
-  $(".walking").css({opacity: "1"});
-}
-  if(s % 10 === 0) {
-  addH.innerHTML = "You Walked: "+count+" minutes in the past "+hoursCount + " Minutes";
-    hoursCount+= 30;
+  
+      hoursCount+= 10;
+
+  addH.innerHTML = "You Walked: "+count+" minutes out of "+hoursCount + " Minutes";
    
-}
+
   if (isChecked === true) {
     $( "#main" ).prepend( div );
     // div.insertBefore(div);
     console.log("true");
+
+
 } else {
   console.log("First Time!");
   $( "#main" ).prepend( div );
@@ -75,13 +69,14 @@ div.className = 'box';
   
   
   console.log(h + ':' + m + ':' + s);
-  shoot.play();
-  
-}
+
 
 }
 
-setInterval(checkTime, 1000);
+
+checkTime();
+setInterval(checkTime, 10000);
+
 
 
 // function checkTime() {
